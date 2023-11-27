79fa0fca3d6ea494ffbb4ef01f5f5875c8d6218f
KozielGPC <gpcgabriel0@gmail.com>
2023-11-27 16:03:23 -0300

    add: swagger docs for the auth route

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -6,6 +6,42 @@ const authRoutes = Router();
 
 const authController = new AuthController();
 
+/**
+ * @swagger
+ * /auth/validate:
+ *   post:
+ *     tags: [Authentication]
+ *     summary: Validate API Key
+ *     description: Validate an API Key and generate a token.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             apiKey: "your_api_key_here"
+ *           schema:
+ *             type: object
+ *             properties:
+ *               apiKey:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: API Key validation successful.
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "API KEY validation successful"
+ *               data:
+ *                 isValid: true
+ *                 token: "generated_token_here"
+ *       500:
+ *         description: Internal server error during API Key validation.
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "Error validating API KEY"
+ *               error: "Internal Server Error"
+ */
 authRoutes.post("/auth/validate", logger, authController.validate);
 
 export { authRoutes };
